d2ea6681880f0c1004b932fe8b53184a16b9e319
ivan-teotonio <ivanteotonio256@gmail.com>
2025-10-26 01:36:17 -0300

    Desenvolvida  estilização do componente de logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,8 +5,8 @@ import styles from './style.module.css';
 
 export function Logo() {
     return <div className={styles.logo}>
-        <a href="">
-            <TimerIcon />
+        <a href="#" className={styles.logoLink}>
+            <TimerIcon size={64} />
             <span>Chronos</span>
         </a>
     </div>
