4faa3cc24a16d99f6784ed7d5c9d700d951742e4
ivan-teotonio <ivanteotonio256@gmail.com>
2025-11-05 17:34:05 -0300

    Criação da lógica da mudança de tema

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,35 @@ import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
+import { Heading } from './components/Heading';
 import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
+  // Que todos os componentes que usan numero 
+  // saibam das mudanças em seu valor
+  //sempre que eu usar useState, não vou usar atribuição diretamente
+  // const [numero, setNumero] = useState(() => {
+  //   console.log('Executou')
+  //   return 0;
+  // });
+  // const [numero, setNumero] = useState(0);
+
+  // function handleClick() {
+  //   //se quiser utilizar o valor anterior, precisa do callback
+  //    //setNumero((prevState) => prevState + 1);
+
+  //    //se não vai utilizar o valor anterior, não precisa do callback
+  //    setNumero(1);
+  // }
   return (
     <>
+
+    {/* <Heading>Número:  {numero}</Heading>
+    <button onClick={handleClick}>Aumentar</button> */}
+
       <Container>
         <Logo />
       </Container>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,63 @@
-import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon, MoonIcon } from 'lucide-react';
 import styles from './style.module.css';
+import { useState, useEffect } from 'react';
+
+
+type AvailableThemes = 'dark' | 'light'
 
 export function Menu() {
+
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+        // ...existing code...
+    const storageTheme = (localStorage.getItem('theme') as AvailableThemes) ?? null;
+    const st = storageTheme ?? 'light';
+    return st;
+    // ...existing code...
+  });
+
+  function handleThemeChage(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+     setTheme(prevTheme => {
+       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+       return nextTheme;
+     });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+    return () => {
+      console.log('Olha, este componente será atualizado')
+    }
+  }, [theme]);//array de dependências, so executa quando o tema muda
+
+  //   useEffect(() => {
+  //   console.log('useEffect sem dependências', Date.now());
+  // });//executa todas vez que o componente é renderizado na tela
+
+  //   useEffect(() => {// bom para api 
+  //   console.log('useEffect sem dependências unica', Date.now());
+  // }, []);//executa apenas uma vez, pela primeira vez que o componente é renderizado na tela
+
+  const nextTheme = {
+    dark: <SunIcon size={64} aria-label='Mudar Tema' />,
+    light: <MoonIcon size={64} aria-label='Mudar Tema' />
+  }
+
   return (
     <nav className={styles.menu}>
-      <a href='#' className={styles.menuLink}>
-        <HouseIcon size={64} />
+      <a href='#' className={styles.menuLink} title='Ir para a Home'>
+        <HouseIcon size={64} aria-label='Ir para a Home' />
       </a>
-      <a href='#' className={styles.menuLink}>
-        <HistoryIcon size={64} />
+      <a href='#' className={styles.menuLink} title='Ir para o histórico'>
+        <HistoryIcon size={64} aria-label='Ir para o histórico' />
       </a>
-      <a href='#' className={styles.menuLink}>
-        <SettingsIcon size={64} />
+      <a href='#' className={styles.menuLink} title='Ir para as configurações'>
+        <SettingsIcon size={64} aria-label='Ir para as configurações' />
       </a>
-      <a href='#' className={styles.menuLink}>
-        <SunIcon size={64} />
+      <a href='#' className={styles.menuLink} title='Mudar Tema' 
+         onClick={handleThemeChage}>
+        {nextTheme[theme]}
       </a>
     </nav>
   );
